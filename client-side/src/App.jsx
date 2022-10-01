0733997e489787b31454027f239fd2c80f1c48a9
soyeba3 <soyebchandani@gmail.com>
2022-10-01 11:50:54 +0600

    pull from misuk

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,11 +1,7 @@
 import Home from "./pages/Home";
 
 const App = () => {
-<<<<<<< HEAD
-  return <div>This is Soyeb here.</div>;
-=======
   return <Home></Home>;
->>>>>>> 043137d6ac48031815229d5623e70d39e726f90a
 };
 
 export default App;
